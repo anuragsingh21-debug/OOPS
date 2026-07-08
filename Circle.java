edba7785b275548affc3c3b5dbf57696c42ca330
anuragsingh21-debug <anuragsingh3406@gmail.com>
2026-07-08 22:05:35 +0530

    child of shapes (overriding)

---
+++ Circle.java
@@ -0,0 +1,8 @@
+// package polymorphism;
+
+public class Circle extends Shapes{
+
+    void area(){//this area overrides the Shapes(parent's class) area
+        System.out.println("circle");
+    }
+}
---
+++ Square.java
@@ -0,0 +1,7 @@
+// package polymorphism;
+
+public class Square extends Shapes{
+    void area()   {//this area overrides the Shapes(parent's class) area
+        System.out.println("square");
+    }
+}
